f550beeb8c1b65faef42ad15de4a9b188cb87bd9
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2025-12-16 23:47:58 -0300

    refactor: Standardize QuestionData property names

--- app/src/main/java/dev/rodrigo/levelupbunpo/domain/data/QuestionData.kt
+++ app/src/main/java/dev/rodrigo/levelupbunpo/domain/data/QuestionData.kt
@@ -4,9 +4,9 @@ data class QuestionData(
     val grammar_point_id: Int,
     val japanese_question: String,
     val correct_option: String,
-    val Incorrect_option_one: String,
-    val Incorrect_option_two: String,
-    val Incorrect_option_three: String,
+    val incorrect_option_one: String,
+    val incorrect_option_two: String,
+    val incorrect_option_three: String,
     val japanese_answer: String,
     val english_translation: String
 )

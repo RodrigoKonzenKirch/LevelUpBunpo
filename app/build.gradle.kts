0c9763333d62f2d647726b939b905d8aa43c3c77
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2025-12-16 21:37:38 -0300

    feat: Add Compose Animation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.animation)
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
